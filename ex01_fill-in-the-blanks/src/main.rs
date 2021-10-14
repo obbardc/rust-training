fc79b267c74d966e239660faf93084d6dbf54021
Christopher Obbard <chris.obbard@collabora.com>
2021-10-14 17:27:36 +0100

    ex01: fix the problem!
    Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

--- ex01_fill-in-the-blanks/src/main.rs
+++ ex01_fill-in-the-blanks/src/main.rs
@@ -11,15 +11,34 @@ fn what_does_the_animal_say(animal: &FarmAnimal) {
 
     /* TODO: fill in the match statement below to make this code compile */
 
+    /*
+    Expected output:
+
+    Dog { name: "Lassie" } says: "woof, woof! I am Lassie!"
+    ​Cow says: "moo"
+    ​Bull says: "moo"
+    ​Chicken { num_eggs: 3 } says: "cluck, cluck!"
+    ​Worm says: "-- (silence)
+    */
+
     let noise = match animal {
-        /* Cow and Bull */ => "moo".to_string(),
-        /* Chicken      */ => "cluck, cluck!".to_string(),
-        /* Dog          */  => format!("woof, woof! I am {}!", name),
-        /* Worm– or all silent animals?*/ => "-- (silence)".to_string(),
+        /* Cow and Bull */
+        FarmAnimal::Cow | FarmAnimal::Bull => "moo".to_string(),
+
+        /* Chicken */
+        FarmAnimal::Chicken { num_eggs: _ } => "cluck, cluck!".to_string(),
+
+        /* Dog */
+        FarmAnimal::Dog { name } => match name.as_str() {
+            "Lassie" => String::from("I am special and do not tell you my name."),
+            _ => format!("woof, woof! I am {}!", name),
+        },
+
+        /* Worm– or all silent animals? */
+        _ => "-- (silence)".to_string(),
     };
 
     /* Bonus task: Give Dogs named Lassie a different output */
-
     println!("{:?} says: {:?}", animal, noise);
 }
 
@@ -27,6 +46,10 @@ fn main() {
     what_does_the_animal_say(
         &FarmAnimal::Dog {
             name: "Lassie".to_string()
+    });
+    what_does_the_animal_say(
+        &FarmAnimal::Dog {
+            name: "Babe".to_string()
     });
     what_does_the_animal_say(&FarmAnimal::Cow);
     what_does_the_animal_say(&FarmAnimal::Bull);
